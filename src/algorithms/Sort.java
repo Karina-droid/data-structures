cd6fc10748394cbc515a57097857bc6d54946da9
Karina-droid <meriina.karin@gmail.com>
2022-07-04 21:21:05 -0700

    added algorithms

--- src/algorithms/Sort.java
+++ src/algorithms/Sort.java
@@ -31,4 +31,88 @@ public class Sort {
 			//swap the found minimum with the first element
 			swap(arr, i, min_idx);
 	}}
+	
+	
+	public void insertionSort(int[] arr) {
+		for(int i=1; i<arr.length; i++) {
+			int key = arr[i];
+			int j = i-1;
+			
+			//move els arr[0..i-1] that are greater than key one pos ahead of their cur pos
+			while(j>=0 && arr[j]>key) {
+				arr[j+1] = arr[j];
+				j--;
+			}arr[j+1] = key;
+		}
+	}
+	
+	
+	public void merge(int[] arr, int l, int m, int r) {
+		//find sizes of the two arrays
+		int s1 = m - l + 1;
+		int s2 = r - m;
+		
+		//create temp arrays, copy data to them
+		int[] L = new int[s1];
+		for(int i=0; i<s1; i++) {
+			L[i] = arr[l+i];
+		}
+		int[] R = new int[s2];
+		for(int i=0; i<s2; i++) {
+			R[i] = arr[l+m+i];
+		}
+		
+		//merge the temp arrays
+		int i = 0;
+		int j = 0;
+		int k = l;
+		while(i<s1 && j<s2) {
+			if(L[i]<R[j]) {
+				arr[k] = L[i];
+				i++;
+			}else {
+				arr[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+		//copy remaining els of L[] and R[] if any
+		while(i<s1) {
+			arr[k] = L[i];
+			i++;
+			k++;
+		}
+		while(j<s2) {
+			arr[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+	
+	
+	public void sort(int[] arr, int l, int r) {
+		int m = l + (r - l)/2;
+		sort(arr, l, m);
+		sort(arr, m+1, r);
+		merge(arr, l, m, r);
+	}
+	
+	
+	public int partition(int[] arr, int low, int high) {
+		int pivot = low;
+		low = low + 1;
+		while(low<high) {
+			if(arr[low]<=pivot) low++;
+			else if(arr[high]>pivot) high--;
+			else swap(arr, low, high);
+		}swap(arr, high, pivot);
+		return high;
+	}
+	
+	
+	public void quickSort(int[] arr, int low, int high) {
+		int pivot = partition(arr, low, high);
+		quickSort(arr, low, pivot-1);
+		quickSort(arr, pivot+1, high);
+	}
 }
